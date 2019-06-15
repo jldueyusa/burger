aa5f4c2e5642e3de883ed70513d1e824c457106e
Jennifer Duey <jlduey@gmail.com>
2019-06-14 20:42:06 -0400

    adding connection information

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+//require dependency
+var mysql = require("mysql");
+
+//create connection to the database
+connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "MmSj.01!",
+    database: "burgers_db"
+})
+
+//create the connection and console log to see if it is connected
+connection.connect(function(err){
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id" + connection.threadID);
+});
