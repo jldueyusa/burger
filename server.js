c0197fa86cda09dbcbef2d81cc5eb889c9628342
Jennifer Duey <jlduey@gmail.com>
2019-06-14 20:31:11 -0400

    added static files, body parser, and handlebars

--- server.js
+++ server.js
@@ -8,6 +8,20 @@ var app = express();
 //set up port
 var PORT = process.env.PORT || 3000;
 
+//static files
+app.use(express.static("public"));
+
+//add body parser elements
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//setup handlebars-template engine
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+//add routes 
+
+
 app.listen(PORT, function() {
 console.log("Server listening on: http://localhost:" + PORT);
 })
