bf10e40d35261bf95e95e8d4e47dbd0deb30f06e
Jennifer Duey <jlduey@gmail.com>
2019-06-14 21:22:25 -0400

    added insert

--- config/orm.js
+++ config/orm.js
@@ -1,13 +1,30 @@
 var connection = require("../config/connection");
 
 var orm = {
-    selectALL: function(table, cb){
+    selectALL: function (table, cb) {
         var dbQuery = "SELECT * FROM" + table + ";";
-        connection.query(dbQuery, function(err, res){
+        connection.query(dbQuery, function (err, res) {
             if (err) {
                 throw err;
             }
             cb(res);
-        })
+        });
+    }
+    // need to 'insert' function
+    insertOne: function (table, cols, vals, cb) {
+        var dbQuery = "INSERT INTO " +
+        table + 
+        " (" + 
+        cols.toString() + 
+        ") " + "VALUES (" + 
+        createQmarks(vals.length) + 
+        ") ";
+        console.log(dbQuery);
+        connection.query(dbQuery, function (err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
     }
 };
