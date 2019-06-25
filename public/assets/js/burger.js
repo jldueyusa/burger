3e638593c8a921eae5272fe2276ecaff2a301c33
Jennifer Duey <jlduey@gmail.com>
2019-06-25 01:45:55 -0400

    added ajax

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -11,5 +11,13 @@ $(function(){
             .trim(),
             devoured: 0
         };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(function(){
+            console.log("Added new burger");
+            location.reload();
+        })
     });
 });
