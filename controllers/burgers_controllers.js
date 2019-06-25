ae2f86f42539697a5a1fbe481ab62c94431d1c53
Jennifer Duey <jlduey@gmail.com>
2019-06-25 00:47:05 -0400

    added post to controller

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -13,4 +13,14 @@ router.get("/", function(req, res){
         console.log(hdbrsObj);
         res.render("index", hdbrsObj);
     });
+
+    router.post("/api/burgers", function(req, res){
+        burger.insertOne(
+            ["burger_name", "devoured"],
+            [req.body.burger_name, req.body.devoured],
+            function(result){
+                res.json({ id: result.insertId});
+            }
+        );
+    });
 });
