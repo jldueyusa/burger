c3ee40cc2bb6ceb4457dd90e29d6b98e12fcf8cd
Jennifer Duey <jlduey@gmail.com>
2019-06-25 00:32:15 -0400

    added the rest of the functions on burgerjs

--- models/burger.js
+++ models/burger.js
@@ -12,11 +12,15 @@ orm.selectAll("burgers", function(res){
             cb(res);
         });
     }
-    updateOne: function(cols, vals, cb) {
-        orm.selectAll("burgers", cols, vals, function(res){
+    updateOne: function(objColVals, condition, cb) {
+        orm.selectAll("burgers", objColVals, condition, function(res){
             cb(res);
         });
     }
-
+    deleteOne: function(condition, cb) {
+        orm.selectAll("burgers", condition, function(res){
+            cb(res);
+        });
+    }
 };
 module.exports = burger;
