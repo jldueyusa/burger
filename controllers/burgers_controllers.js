c294dfb3df53f9383afa66875d33071265237e50
Jennifer Duey <jlduey@gmail.com>
2019-06-25 01:15:01 -0400

    added delete router

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -31,12 +31,24 @@ router.put("/api/burgers/:id", function(req, res){
     burger.updateOne({ devoured: req.body.devoured }, condition, function(
         result
     ) {
-        if (result.changeRows === 0) {
+        if ((result.changeRows === 0)) {
             return res.status(404).end();
         } else {
           res.status(200).end();
         }
     });
 });
+router.deleteOne(condition, function(req, res){
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
 
+    burger.deleteOne(condition, function(result){
+        if ((result, changeRows === 0)) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    })
+})
 });
+module.exports = router;
