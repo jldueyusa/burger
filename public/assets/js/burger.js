e6e7b19e842916d919768cb5fbf246e789aa338f
Jennifer Duey <jlduey@gmail.com>
2019-06-25 01:29:58 -0400

    created js folder and started new burgerjs file

---
+++ public/assets/js/burger.js
@@ -0,0 +1,7 @@
+//this file contains the ajax that calls all the onclick which triggers the actions
+$(function(){
+
+    $(".create-form").on("submit", function(event){
+        event.preventDefault();
+    });
+});
