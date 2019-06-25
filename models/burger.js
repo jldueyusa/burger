de7ba02d1dd8e87370792db1015942b626a47000
Jennifer Duey <jlduey@gmail.com>
2019-06-25 01:17:36 -0400

    fixed insert, update, and deleteone on burger js

--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,4 @@
-var orm = require("../config/orm");
+var orm = require("../config/orm.js");
 
 var burger = {
     // call back all the ORMs
@@ -6,17 +6,17 @@ var burger = {
 orm.selectAll("burgers", function(res){
     cb(res);
 });
-    }
+    },
     insertOne: function(cols, vals, cb) {
         orm.selectAll("burgers", cols, vals, function(res){
             cb(res);
         });
-    }
+    },
     updateOne: function(objColVals, condition, cb) {
         orm.selectAll("burgers", objColVals, condition, function(res){
             cb(res);
         });
-    }
+    },
     deleteOne: function(condition, cb) {
         orm.selectAll("burgers", condition, function(res){
             cb(res);
