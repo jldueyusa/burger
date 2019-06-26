4a3a8b7542e38727e526f60d8e7c18bcb8288391
Jennifer Duey <jlduey@gmail.com>
2019-06-26 19:03:45 -0400

    trouble shooting uninstalled node_modules, added git ignore, re-installed my packages

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,14 @@
 //require dependency
-var mysql = require("mysql");
-
-//create connection to the database
-if(process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else{
-connection = mysql.createConnection({
+const mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "MmSj.01!",
     database: "burgers_db"
-});
-};
-
-//create the connection and console log to see if it is connected
-connection.connect(function(err){
-    if (err){
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadID);
-});
-
+  });
+}
+connection.connect();
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,28 +1,21 @@
-//dependencies
-const express = require("express");
-const bodyParser = require("body-parser");
-const exphbs = require("express-handlebars");
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 
-const app = express();
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-//set up port
-const PORT = process.env.PORT || 9595;
-
-//static files
 app.use(express.static("public"));
 
-//add body parser elements
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//setup handlebars-template engine
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-//add routes 
-const routes = require("./controllers/burgers_controllers.js");
+var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 app.listen(PORT, function() {
-console.log("Server listening on: http://localhost:" + PORT);
+  console.log("Server listening on: http://localhost:" + PORT);
 });
